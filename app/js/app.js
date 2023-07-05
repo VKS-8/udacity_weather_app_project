d6f412025641ff42c4a12b28efbfa6b9b7661bb2
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-07-04 21:42:49 -0500

    Scrap and rewrote basic setup

--- app/js/app.js
+++ app/js/app.js
@@ -5,36 +5,49 @@ import './css/styles.css';
 /* Global Variables */
 
 // Personal API Key for OpenWeatherMap API
-const apiKey = '<your_api_key>&units=imperial';
+const API_KEY = '<your_api_key>&units=imperial';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const zip = document.querySelector('#zip').value;
 
 // Business Logic
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
-document.addEventListener('DOMContentLoaded', funtion() {
-//TODO add appropriate calls here
-onSubmit();
-
-});
-
-function getCurrentWeather() {
-  let request = new XMLHttpRequest();
-
-  request.addEventListener('loadend', ()=> {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printResults(response, zip);
-    } else {
-      printError(this, response, zip);
+// Get data from OpenWeatherMap API
+async function getCurrentWeather() {
+  const url = `${API_URL}?zip=${zip}&appid=${API_KEY}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not successful.');
     }
-  });
-
-  request.open("GET", apiKey, true);
-  request.send();
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    throw error;
+  }
 }
 
+// Post data to the server
+async function postData(url = '', data = {}) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return await response.json();
+  } catch (error) {
+      console.error('Error posting data: ', error);
+      throw error;
+  }
+}
 
 // UI Logic
 
@@ -43,44 +56,44 @@ function printResults(apiResponse, zip) {
   document.querySelector('#content').innerText = allData.feel;
   document.querySelector('#date').innerText = allData.date;
 
- // document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
-  // document.getElementById('content').innerHTML = allData.feel;
-  // document.getElementById("date").innerHTML =allData.date;
 }
 
-function onSubmit(event) {
-  const submitBtn = document.querySelector('#generate');
-
-  event.prevent('default');
-
-  submitBtn.addEventListener('click',submitInput);
-
-  const submitInput = (event) => {
-    const zip = document.querySelector('#zip').value;
-    document.querySelector('#zip').value=null;
-    getCurrentWeather(zip);
-    console.log("submitted");
-  };
-
-  submitBtn.removeEventListener('click',submitInput);
-  submitBtn.addEventListener('click',submitInput);
+function printError(request, apiResponse, zip) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
-const retrieveData = async () =>{
-  const request = await fetch('/all');
+async function generateBtnHandler() {
+  const zipInput = document.querySelector('#zip').value;
+  const feelingsInput = document.querySelector('feelingsInput').value;
+
   try {
-  // Transform into JSON
-  const allData = await request.json()
-  console.log(allData)
-  // Write updated data to DOM elements
-  printResults();
-  // document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
-  // document.getElementById('content').innerHTML = allData.feel;
-  // document.getElementById("date").innerHTML =allData.date;
+    const weatherData = await getCurrentWeather(zipInput);
+    const temp = weatherData.main.temp;
+    const date = new Date().toLocaleDateString();
 
+    // Create JS object with fetched data and user input
+    const projectData = {
+      temperature: temp,
+      date: date,
+      userResponse: feelingsInput,
+    }
+
+    // Post data to the server
+    await postData('/saveData', projectData);
+
+    // Update the UI witht he fetched data
+    updateUI(projectData);
+  } catch (error) {
+    console.error('Error generating data: ', error);
   }
-  catch(error) {
-    console.log("error", error);
-    // appropriately handle the error
-  }
- }
+}
+
+// Event listener for the generate button
+document.querySelector('#generate').addEventListener('click', generateBtnHandler);
+
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+  //TODO add appropriate calls here
+
+  });
---
+++ server.js
@@ -0,0 +1,30 @@
+// Express to run server and routes
+const express = require('express');
+const cors = require('cors'); // Cross Origin Resource Sharing
+const app = express(); // Start an instance of the app
+const port = 5500;
+
+app.use(cors());
+
+// Parse JSON bodies
+app.use(express.json());
+
+// /* Dependencies */
+// const bodyParser = require('body-parser');
+
+// /* Middle-ware */
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+// Initialize the main project folder
+app.use(express.static('app'));
+
+app.post('/saveData', (req, res) => {
+  const data = req.body; //Access the parsed JSON data sent by the client
+  console.log(data); // Log the JSON data to the server console
+  res.status(200).json({ message: 'Data saved successfully.'});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost: ${port}`);
+});
